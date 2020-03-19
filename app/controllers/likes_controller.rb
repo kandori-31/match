43c417dd5eb85f34f29580ee3d6e926e7c88f1d9
kandori-31 <ezynmthd@i.softbank.jp>
2020-03-19 14:07:40 +0900

    フォロー、いいね機能

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,5 @@
+class LikesController < ApplicationController
+  def index
+    @users = User.all
+  end
+end
---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,22 @@
+class RelationshipsController < ApplicationController
+  before_action :set_user
+
+  def create
+    user = User.find(params[:relationship][:follow_id])
+    following = current_user.follow(user)
+    redirect_to user
+  end
+
+  def destroy
+    user = User.find(params[:relationship][:follow_id])
+    following = current_user.unfollow(user)
+    redirect_to user
+  end
+
+  private
+
+  def set_user
+    user = User.find(params[:relationship][:follow_id])
+  end
+
+end
