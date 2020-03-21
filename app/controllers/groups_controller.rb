3821b8fb1460a3835fedec65810013442a0d2a46
kandori-31 <ezynmthd@i.softbank.jp>
2020-03-21 23:45:24 +0900

    ストロングパラメーター

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.create
     @entry1 = GroupUser.create(group_id: @group.id, user_id: current_user.id)
-    @entry2 = GroupUser.create(params.require(:group_user).permit(:user_id, :group_id).merge(group_id: @group.id))
+    @entry2 = GroupUser.create(group_params)
       redirect_to group_path(@group.id)
   
   end
@@ -17,6 +17,10 @@ class GroupsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+  private
 
+  def group_params
+    params.require(:group_user).permit(:user_id, :group_id).merge(group_id: @group.id)
+  end
   
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,7 @@ class MessagesController < ApplicationController
 
   def create 
     if GroupUser.where(user_id: current_user.id, group_id: params[:message][:group_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :group_id).merge(user_id: current_user.id))
-      
+      @message = Message.create(message_params)
     end
     redirect_to group_path(@message.group_id)
 end
@@ -12,9 +11,6 @@ end
   private
 
   def message_params
-    params.require(:message).permit(:content,).merge(user_id: current_user.id)
-  end
-  def set_group
-    @group = Group.find(params[:group_id])
+    params.require(:message).permit(:user_id, :content, :group_id).merge(user_id: current_user.id)
   end
 end
