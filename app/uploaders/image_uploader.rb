d7c44bc7566241785d143bfe5eb47013db18ccc9
kandori-31 <ezynmthd@i.softbank.jp>
2020-04-23 23:19:25 +0900

    S3関連コメントアウト

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,26 +4,26 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if  Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if  Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        # Amazon S3用の設定
-        :provider              => 'AWS',
-        :region                => ENV['S3_REGION'],  # S3に設定したリージョン。
-        :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-        :aws_secret_access_key => ENV['S3_SECRET_KEY']
-      }
-      config.fog_directory     =  ENV['S3_BUCKET']
-      end
-    end
+  # if Rails.env.production?
+  #   CarrierWave.configure do |config|
+  #     config.fog_credentials = {
+  #       # Amazon S3用の設定
+  #       :provider              => 'AWS',
+  #       :region                => ENV['S3_REGION'],  # S3に設定したリージョン。
+  #       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+  #       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+  #     }
+  #     config.fog_directory     =  ENV['S3_BUCKET']
+  #     end
+  #   end
 
-  # storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
